b5c66c3e82ec818a73b468459cffcf6e838ea8c8
zhang BaiXuan20222118 <zhangbaixuan2022@outlook.com>
2025-12-09 09:17:18 +0700

    2025/12/9

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import './index.css';
 import './styles/global.css';
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
